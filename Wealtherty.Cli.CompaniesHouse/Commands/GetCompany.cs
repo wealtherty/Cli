5fe0769e7df3d497f7690d98e7c12f02c90c29e8
swotlycto <137119161+wealtherty@users.noreply.github.com>
2023-06-20 00:55:20 +0100

    Update GetCompany.cs

--- Wealtherty.Cli.CompaniesHouse/Commands/GetCompany.cs
+++ Wealtherty.Cli.CompaniesHouse/Commands/GetCompany.cs
@@ -13,7 +13,7 @@ public class GetCompany : Command
 {
     [Option('n', "number", Required = true)]
     public string Number { get; set; }
-    
+
     protected override async Task ExecuteImplAsync(IServiceProvider serviceProvider)
     {
         var companiesHouseClient = serviceProvider.GetService<ICompaniesHouseClient>();
@@ -21,22 +21,26 @@ public class GetCompany : Command
         var driver = serviceProvider.GetService<IDriver>();
 
         await using var session = driver.AsyncSession();
-        
+
         var getOfficersResponse = await companiesHouseClient.GetOfficersAsync(Number, 0, 250);
 
         foreach (var officer in getOfficersResponse.Data.Items)
         {
             var officerNode = mapper.Map<Officer>(officer);
+
+            var getAppointmentsResponse =
+                await companiesHouseClient.GetAppointmentsAsync(officer.Links.Officer.OfficerId, 0, 250);
             
-            var getAppointmentsResponse = await companiesHouseClient.GetAppointmentsAsync(officer.Links.Officer.OfficerId, 0, 250);
             foreach (var appointment in getAppointmentsResponse.Data.Items)
             {
-                var getAppointmentCompanyResponse = await companiesHouseClient.GetCompanyProfileAsync(appointment.Appointed.CompanyNumber);
+                var getAppointmentCompanyResponse =
+                    await companiesHouseClient.GetCompanyProfileAsync(appointment.Appointed.CompanyNumber);
                 var appointmentCompanyNode = mapper.Map<Company>(getAppointmentCompanyResponse.Data);
 
                 officerNode.AddRelation(new Appointment(officerNode, appointmentCompanyNode, appointment));
             }
-            
+
             await session.ExecuteCommandsAsync(officerNode);
-        }    }
+        }
+    }
 }
