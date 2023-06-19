083992acb57528de2fdae7b66a962c6cd0f30b6f
swotlycto <137119161+wealtherty@users.noreply.github.com>
2023-06-20 00:52:20 +0100

    Updated Mapping

---
+++ Wealtherty.Cli.CompaniesHouse/Mapping/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CompaniesHouse.Response.CompanyProfile;
+using Wealtherty.Cli.CompaniesHouse.Mapping.Resolvers;
+using Wealtherty.Cli.CompaniesHouse.Model;
+
+namespace Wealtherty.Cli.CompaniesHouse.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CompanyProfile, Company>()
+            .ForMember(x => x.Name, opt => opt.MapFrom(x => x.CompanyName))
+            .ForMember(x => x.Number, opt => opt.MapFrom(x => x.CompanyNumber))
+            .ForMember(x => x.Status, opt => opt.MapFrom(x => x.CompanyStatus.ToString()))
+            .ForMember(x => x.Status, opt => opt.MapFrom(x => x.CompanyStatus.ToString()))
+            .ForMember(x => x.CreatedOn, opt => opt.MapFrom(x => x.DateOfCreation))
+            .ForMember(x => x.StoppedTradingOn, opt => opt.MapFrom(x => x.DateOfCessation));
+        
+        CreateMap<global::CompaniesHouse.Response.Officers.Officer, Officer>()
+            .ForMember(x => x.Name, opt => opt.MapFrom<NameResolver>())
+            .ForMember(x => x.OfficerId, opt => opt.MapFrom(x => x.Links.Officer.OfficerId))
+            .ForMember(x => x.YearOfBirth, opt => opt.MapFrom(x => x.DateOfBirth.Year));
+    }
+}
---
+++ Wealtherty.Cli.CompaniesHouse/Mapping/Resolvers/NameResolver.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Wealtherty.Cli.CompaniesHouse.Model;
+
+namespace Wealtherty.Cli.CompaniesHouse.Mapping.Resolvers;
+
+public class NameResolver : IValueResolver<global::CompaniesHouse.Response.Officers.Officer, Officer, string>
+{
+    public string Resolve(global::CompaniesHouse.Response.Officers.Officer source, Officer destination, string destMember, ResolutionContext context)
+    {
+        var parts = source.Name.Split(", ");
+        Array.Reverse(parts);
+        var reversed = string.Join(" ", parts);
+        return reversed;
+    }
+}
