68108e842c81ddb74589503b06a7e4d2b64cc112
swotlycto <137119161+wealtherty@users.noreply.github.com>
2023-06-20 00:46:46 +0100

    Update GetCompany.cs

--- Wealtherty.Cli.CompaniesHouse/Commands/GetCompany.cs
+++ Wealtherty.Cli.CompaniesHouse/Commands/GetCompany.cs
@@ -3,7 +3,6 @@ using CommandLine;
 using CompaniesHouse;
 using Microsoft.Extensions.DependencyInjection;
 using Neo4j.Driver;
-using Serilog;
 using Wealtherty.Cli.CompaniesHouse.Model;
 using Wealtherty.Cli.Core;
 
@@ -20,14 +19,24 @@ public class GetCompany : Command
         var companiesHouseClient = serviceProvider.GetService<ICompaniesHouseClient>();
         var mapper = serviceProvider.GetService<IMapper>();
         var driver = serviceProvider.GetService<IDriver>();
-        
-        await using var session = driver.AsyncSession();
 
-        var respone = await companiesHouseClient.GetCompanyProfileAsync(Number);
-        var companyNode = mapper.Map<Company>(respone.Data);
+        await using var session = driver.AsyncSession();
         
-        await session.ExecuteCommandsAsync(companyNode);
-        
-        Log.Information("{@Company}", companyNode);
-    }
+        var getOfficersResponse = await companiesHouseClient.GetOfficersAsync(Number, 0, 250);
+
+        foreach (var officer in getOfficersResponse.Data.Items)
+        {
+            var officerNode = mapper.Map<Officer>(officer);
+            
+            var getAppointmentsResponse = await companiesHouseClient.GetAppointmentsAsync(officer.Links.Officer.OfficerId, 0, 250);
+            foreach (var appointment in getAppointmentsResponse.Data.Items)
+            {
+                var getAppointmentCompanyResponse = await companiesHouseClient.GetCompanyProfileAsync(appointment.Appointed.CompanyNumber);
+                var appointmentCompanyNode = mapper.Map<Company>(getAppointmentCompanyResponse.Data);
+
+                officerNode.AddRelation(new Appointment(officerNode, appointmentCompanyNode, appointment));
+            }
+            
+            await session.ExecuteCommandsAsync(officerNode);
+        }    }
 }
