8a04e9d83d459c66868fcad2863f95bb85b51f2d
swotlycto <yw8cq3@pm.me>
2023-06-29 09:34:53 +0100

    Added retries

--- Wealtherty.Cli.CompaniesHouse/Client.cs
+++ Wealtherty.Cli.CompaniesHouse/Client.cs
@@ -1,12 +1,30 @@
 ﻿using CompaniesHouse;
 using CompaniesHouse.Response.Appointments;
 using CompaniesHouse.Response.Officers;
+using Polly;
+using Polly.Retry;
 using Serilog;
 
 namespace Wealtherty.Cli.CompaniesHouse;
 
 public class Client
 {
+    private readonly AsyncRetryPolicy _retry = Policy
+        .Handle<HttpRequestException>(x => !x.Message.Contains("404"))
+        .WaitAndRetryAsync(new[]
+        {
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(1)
+        }, (exception, span) => Log.Error(exception, "Error calling Companies House - Sleeping for: {@Span}", span));
+
     private readonly ICompaniesHouseClient _companiesHouseClient;
 
     public Client(ICompaniesHouseClient companiesHouseClient)
@@ -24,7 +42,9 @@ public class Client
 
         do
         {
-            var response = await _companiesHouseClient.GetOfficersAsync(companyNumber, startIndex, pageSize);
+            var policyResult = await _retry.ExecuteAndCaptureAsync(() => _companiesHouseClient.GetOfficersAsync(companyNumber, startIndex, pageSize));
+            var response = policyResult.Result;
+            
             officers.AddRange(response.Data.Items);
             expected = response.Data.ActiveCount.Value + response.Data.ResignedCount.Value;
             startIndex += pageSize;
@@ -47,7 +67,9 @@ public class Client
 
         do
         {
-            var response = await _companiesHouseClient.GetAppointmentsAsync(officerId, startIndex, pageSize);
+            var policyResult = await _retry.ExecuteAndCaptureAsync(() => _companiesHouseClient.GetAppointmentsAsync(officerId, startIndex, pageSize));
+            var response = policyResult.Result;
+
             appointmemts.AddRange(response.Data.Items);
             expected = response.Data.TotalResults;
             startIndex += pageSize;
