78b8942e23e1bf3658b27add41a8056f4375fc9e
swotlycto <yw8cq3@pm.me>
2023-06-29 09:28:51 +0100

    GetCompanies

---
+++ Wealtherty.Cli.CompaniesHouse/Commands/GetCompanies.cs
@@ -0,0 +1,66 @@
+﻿using CommandLine;
+using CompaniesHouse;
+using Microsoft.Extensions.DependencyInjection;
+using Neo4j.Driver;
+using Wealtherty.Cli.CompaniesHouse.Model;
+using Wealtherty.Cli.Core;
+
+namespace Wealtherty.Cli.CompaniesHouse.Commands;
+
+[Verb("ch:companies-get")]
+public class GetCompanies : Command
+{
+    private class CompanyNumbers
+    {
+        
+        private const string AdamSmithInstitute = null;
+        private const string BowGroup = null;
+        private const string CentreForPolicyStudies = "01174651";
+        private const string CentreForSocialJustice = "05137036";
+        private const string InstitueOfEconomicAffairs = null;
+        private const string PolicyExchange = null;
+        private const string Reform = null;
+        private const string LegatumInstitute = null;
+        
+        public static IEnumerable<string> All()
+        {
+            return new[]
+            {
+                CentreForSocialJustice,
+            };
+        }
+    }
+    
+    protected override async Task ExecuteImplAsync(IServiceProvider serviceProvider)
+    {
+        var companiesHouseClient = serviceProvider.GetService<ICompaniesHouseClient>();
+        var client = serviceProvider.GetService<Client>();
+        var driver = serviceProvider.GetService<IDriver>();
+
+        await using var session = driver.AsyncSession();
+
+        foreach (var companyNumber in CompanyNumbers.All().Where(x => x != null))
+        {
+            var officers = await client.GetOfficersAsync(companyNumber);
+
+            foreach (var officer in officers)
+            {
+                var officerNode = new Officer(officer);
+
+                var appointments = await client.GetAppointmentsAsync(officer.Links.Officer.OfficerId);
+            
+                foreach (var appointment in appointments)
+                {
+                    var getAppointmentCompanyResponse = await companiesHouseClient.GetCompanyProfileAsync(appointment.Appointed.CompanyNumber);
+                    var appointmentCompanyNode = new Company(getAppointmentCompanyResponse.Data);
+
+                    officerNode.AddRelation(new Appointment(officerNode, appointmentCompanyNode, appointment));
+                }
+
+                await session.ExecuteCommandsAsync(officerNode);
+            }
+            
+        }
+
+    }
+}
--- Wealtherty.Cli/Program.cs
+++ Wealtherty.Cli/Program.cs
@@ -8,8 +8,9 @@ namespace Wealtherty.Cli;
 public static class Program
 {
     private static int Main(string[] args) {
-        return Parser.Default.ParseArguments<GetCompany, SearchProjects>(args)
+        return Parser.Default.ParseArguments<GetCompanies, GetCompany, SearchProjects>(args)
             .MapResult(
+                (GetCompanies command) => Execute(command),
                 (GetCompany command) => Execute(command),
                 (SearchProjects command) => Execute(command),
                 _ => 1);
