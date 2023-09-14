74c40e9edd99ff3f80855d8677cff9e0347c9b6d
swotlycto <yw8cq3@pm.me>
2023-09-14 10:01:36 +0100

    OutputWriter creates new folder per command run

--- Wealtherty.Cli.Core/OutputWriter.cs
+++ Wealtherty.Cli.Core/OutputWriter.cs
@@ -5,11 +5,20 @@ namespace Wealtherty.Cli.Core;
 
 public class OutputWriter
 {
+    private readonly string _dir;
+    public OutputWriter()
+    {
+        _dir = $"output\\{DateTime.Now.ToString("yyyyMMdd_hhmmss")}\\";
+    }
+    
     public async Task WriteToCsvFileAsync<T>(IEnumerable<T> rows, string path)
     {
-        await using var writer = new StreamWriter(path.Replace(' ', '_'));
+        var fullPath = Path.Join(_dir, path).Replace(' ', '_');
+
+        Directory.CreateDirectory(_dir);
+        
+        await using var writer = new StreamWriter(fullPath);
         await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
         await csv.WriteRecordsAsync(rows);
     }
-
 }
