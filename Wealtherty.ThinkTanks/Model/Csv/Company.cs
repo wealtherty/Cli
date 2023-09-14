f0462aedc08587fe9d022376064af45a2f11f845
swotlycto <yw8cq3@pm.me>
2023-09-14 09:48:43 +0100

    Update Company.cs

--- Wealtherty.ThinkTanks/Model/Csv/Company.cs
+++ Wealtherty.ThinkTanks/Model/Csv/Company.cs
@@ -4,12 +4,5 @@ public class Company
 {
     public string OttId { get; set; }
     
-    public string ThinkTank { get; set; }
-    
-    public string Website { get; set; }
- 
     public string CompanyNumber { get; set; }
-    
-    public string Notes { get; set; }
-    
 }
